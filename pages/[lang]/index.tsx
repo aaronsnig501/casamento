ec34ac525d0e66571d3a74bd57e83ddf9fb287e7
Aaron Sinnott <aaronsnig@gmail.com>
2020-08-11 17:45:53 -0300

    Add some basic styling

--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -1,12 +1,24 @@
 import Head from "next/head";
 import { NextPage } from "next";
+import { Container, Row, Col } from "react-bootstrap";
 import withLocale from "../../i18n/hoc/withLocale";
 import useTranslation from "../../i18n/hooks/useTranslation";
+import styles from "./index.module.scss";
 
 const Home: NextPage = () => {
   const { locale, t } = useTranslation();
 
-  return <h1>{t("mainheader")}</h1>;
+  return (
+    <div className={styles.jumbotron}>
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1>{t("mainheader")}</h1>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default withLocale(Home);
