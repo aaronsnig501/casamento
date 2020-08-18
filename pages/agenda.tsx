c0074d0cf581da98e5b6e55db7ea363597bb13fd
Aaron Sinnott <aaronsnig@gmail.com>
2020-08-18 20:55:33 -0300

    Content and style changes

--- pages/agenda.tsx
+++ pages/agenda.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { Container, Row, Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import styles from "./agenda.module.scss";
 
 const Agenda: NextPage = () => {
   const { t, i18n } = useTranslation();
@@ -9,14 +10,21 @@ const Agenda: NextPage = () => {
     <Container fluid>
       <Row>
         <Col>
-          <a href="https://us04web.zoom.us/j/72821548402?pwd=bUZTOUc2T2l0V1VlaVp3SU8vMytOUT09">
-            Link
-          </a>
-          <h1 />
-          <p>{t("paragraphone")}</p>
-          <p>{t("paragraphtwo")}</p>
-          <p>{t("paragraphthree")}</p>
-          <p>{t("paragraphfour")}</p>
+          <div className={styles.agendaWrapper}>
+            <a href="https://us04web.zoom.us/j/72821548402?pwd=bUZTOUc2T2l0V1VlaVp3SU8vMytOUT09">
+              Link
+            </a>
+            <h1>{t("agendaheading")}</h1>
+            <p>{t("paragraphone")}</p>
+            <p>
+              {t("paragraphtwo")} -{" "}
+              <a href="https://zoom.us/download" target="_blank">
+                link
+              </a>
+            </p>
+            <p>{t("paragraphthree")}</p>
+            <p>{t("paragraphfour")}</p>
+          </div>
         </Col>
       </Row>
     </Container>
